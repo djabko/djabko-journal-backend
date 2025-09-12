6a5452e94fece55d751206dc7662e54ca0731346
djabko <djabko@proton.me>
2025-09-12 21:16:27 +0100

    refactor: add Constants namespace class
    - Provides {raw: label} mapping eg. {'tag1': 'Tag 1'}
    
    - Labels are used for display in dynamodb, analytics, or UI, while raw labels
      are optimized for brevity and tailored to transport formats like json.
    
    - All json input/output now uses raw string constants.

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from datetime import datetime
 from flask import Flask, request, redirect
 from boto3.dynamodb.conditions import Key
 
+from util import *
+
 
 def get_priv(key):
     if key not in os.environ:
@@ -56,103 +58,113 @@ def journal_create():
 
         notebook += ''.join(secrets.choice(alphabet) for _ in range(10))
 
-        kce = Key('Notebook').eq(notebook)
+        kce = Key(Constants.NOTEBOOK.label).eq(notebook)
         items = dynamo_table.query(KeyConditionExpression=kce, Limit=1)
         
         if items['Count'] == 0:
             break
 
-    return json.dumps({"notebook": notebook})
+    return json.dumps({Constants.NOTEBOOK.raw: notebook})
 
 
 @app.delete("/journal/delete")
 def journal_delete():
     data = request.json
 
-    if 'notebook' not in data:
+    if Constants.NOTEBOOK.raw not in data:
         return 'Notebook key not supplied\n'
 
-    notebook = data['notebook']
-    query = boto3.dynamodb.conditions.Key('Notebook').eq(notebook)
+    notebook = data[Constants.NOTEBOOK.raw]
+    query = boto3.dynamodb.conditions.Key(Constants.NOTEBOOK.label).eq(notebook)
     items = dynamo_table.query(KeyConditionExpression=query)['Items']
 
     print(json.dumps(items, indent=2))
 
     with dynamo_table.batch_writer() as bw:
         for item in items:
-            key = {'Notebook': notebook, 'Datetime': item['Datetime']}
+            key = {
+                    Constants.NOTEBOOK.label: notebook,
+                    Constants.DATETIME.label: item[Constants.DATETIME.label]
+            }
 
             bw.delete_item(Key=key)
 
     return json.dumps({'deleted': f'{len(items)}'})
-
-FIELD_TRANS_DICT = {
-    'author': 'Author',
-    'tag1': 'Tag 1',
-    'tag2': 'Tag 2',
-    'tag3': 'Tag 3',
-    'tag4': 'Tag 4',
-}
 
 
 @app.post("/journal/log")
 def journal_log():
     data = request.json
 
-    if 'notebook' not in data or 'message' not in data:
+    if Constants.NOTEBOOK.raw not in data or Constants.MESSAGE.raw not in data:
         return json.dumps({'status': '300', 'error': 'Missing notebook id or message...\n'})
 
     print(data)
 
-    item = {'Notebook': data['notebook'],
-            'Datetime': str(datetime.now()),
-            'Message':  data['message'],
+    item = {Constants.NOTEBOOK.label: data[Constants.NOTEBOOK.raw],
+            Constants.DATETIME.label: str(datetime.now()),
+            Constants.MESSAGE.label: data[Constants.MESSAGE.raw],
            }
 
-    for k in FIELD_TRANS_DICT.keys():
-        if k in data and data[k]:
-            item[FIELD_TRANS_DICT[k]] = data[k]
+    for k in Constants.trans_dict.keys():
+        if k not in item and k in data and data[k]:
+            item[Constants.trans_dict[k]] = data[k]
 
-    print("\nWRITING\n")
     print(json.dumps(item))
 
     r = dynamo_table.put_item(Item=item)
 
-    return json.dumps(item)
+    res = data
+    res[Constants.DATETIME.raw] = item[Constants.DATETIME.label]
+
+    return json.dumps(res)
 
 
 @app.post("/journal/read")
 def journal_read():
-    data = request.json
+    try:
+        data = request.json
 
-    if 'notebook' not in data:
-        return 'Notebook key not supplied\n'
+        if Constants.NOTEBOOK.raw not in data:
+            return 'Notebook key not supplied\n'
 
-    notebook = data['notebook']
-    dt = data['datetime'] if 'datetime' in data else None
+        notebook = data[Constants.NOTEBOOK.raw]
+        dt = data[Constants.DATETIME.raw] if Constants.DATETIME.raw in data else None
 
-    kce = Key('Notebook').eq(notebook)
+        kce = Key(Constants.NOTEBOOK.label).eq(notebook)
 
-    if dt:
-        kce &= Key('Datetime').begins_with(dt)
+        if dt:
+            kce &= Key(Constants.DATETIME.label).begins_with(dt)
 
-    fe = None
+        fe = None
 
-    optional = {'author': 'Author', 'tag1': 'Tag 1', 'tag2': 'Tag 2', 'tag3': 'Tag 3', 'tag4': 'Tag 4'}
+        optional = [
+                Constants.AUTHOR.raw,
+                Constants.TAG1.raw,
+                Constants.TAG2.raw,
+                Constants.TAG3.raw,
+                Constants.TAG4.raw,
+                ]
 
-    for k,v in optional.items():
-        if k in data:
-            condition &= Key(v).begins_with(data[k])
-            fe = condition if fe is None else fe & condition
+        for k in optional:
+            if k in Constants.trans_dict and k in data:
+                v = Constants.trans_dict[k]
+                condition = Key(v).eq(data[k])
+                fe = fe & condition if fe is not None else condition
 
-    if fe:
-        items = dynamo_table.query(KeyConditionExpression=kce, FilterExpression=fe)
-    else:
-        items = dynamo_table.query(KeyConditionExpression=kce)
+        if fe:
+            items = dynamo_table.query(KeyConditionExpression=kce, FilterExpression=fe)
+        else:
+            items = dynamo_table.query(KeyConditionExpression=kce)
 
-    if len(items) <= 0:
-        print('[]')
+        if len(items) <= 0:
+            print('[]')
 
-    print(json.dumps(items, indent=2))
+        print(json.dumps(items, indent=2))
 
-    return json.dumps({'count': items['Count'], 'items': items['Items']})
+        return json.dumps({'count': items['Count'], 'items': items['Items']})
+
+    except Exception as e:
+        print(json.dumps(request.json, indent=2))
+        print(e)
+        return "500 server error"
--- util.py
+++ util.py
@@ -19,3 +19,29 @@ class Notebook:
 
     def read(notebook_id):
         pass
+
+
+class JournalConstant:
+    trans_dict = {}
+
+    def __init__(self, raw, label):
+        self.raw = raw
+        self.label = label
+
+        JournalConstant.trans_dict[raw] = label
+
+    def __str__(self):
+        return f'({self.raw}: {self.label})';
+
+
+class Constants:
+    NOTEBOOK = JournalConstant('notebook', 'Notebook')
+    MESSAGE = JournalConstant('message', 'Message')
+    DATETIME = JournalConstant('datetime', 'Datetime')
+    AUTHOR = JournalConstant('author', 'Author')
+    TAG1 = JournalConstant('tag1', 'Tag 1')
+    TAG2 = JournalConstant('tag2', 'Tag 2')
+    TAG3 = JournalConstant('tag3', 'Tag 3')
+    TAG4 = JournalConstant('tag4', 'Tag 4')
+
+    trans_dict = JournalConstant.trans_dict
